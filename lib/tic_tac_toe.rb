37f9aec914a8be2e0e70611a7c8f3d04be2ea4b5
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-20 17:12:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,7 +85,7 @@ def won?
    WIN_COMBINATIONS.detect do |combo|
    @board[combo[0]] == @board[combo[1]] &&
      @board[combo[1]] == @board[combo[2]] &&
-     position_taken?(@board, combo[0])
+     position_taken?(combo[0])
  end
 
 end
